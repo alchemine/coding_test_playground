c52384bb4cf4658940a9b8215f159a1ee5b26929
djy-git <djyoon0223@gmail.com>
2022-08-03 16:56:08 +0900

    [add] leetcode.py, programmers.py | Submission format

---
+++ leetcode.py
@@ -0,0 +1,12 @@
+def log(*msg, act=True):
+    if act:
+        print(*msg)
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList):
+
+
+
+
+print(Solution().findLadders(beginWord = "hit", endWord = "cog", wordList = ["hot", "dot", "dog", "lot", "log", "cog"]))
---
+++ programmers.py
@@ -0,0 +1,8 @@
+def solution():
+    VERBOSE = True
+    def log(*msg):
+        if VERBOSE: print(*msg)
+    # -----------------------------
+
+
+print(solution())
