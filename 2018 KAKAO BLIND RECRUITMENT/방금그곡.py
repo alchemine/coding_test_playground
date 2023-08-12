c269554c61962b17dcb3fbab67def81d7fc673df
alchemine <djyoon0223@gmail.com>
2023-08-12 13:25:50 +0900

    update

--- 2018 KAKAO BLIND RECRUITMENT/방금그곡.py
+++ 2018 KAKAO BLIND RECRUITMENT/방금그곡.py
@@ -1,38 +1,45 @@
-from datetime import datetime
-from itertools import cycle
-import re
+# 49m 27s
 
+VERBOSE = False
+# VERBOSE = True
+log = lambda *msg: print(*msg) if VERBOSE else None
+
+from datetime import datetime
+from itertools import cycle, islice
+
+
+NOTE_DIC = {f"{note}#": chr(ord('H') + ord(note)-ord('A')) for note in 'ACDFG'}
+
+def convert(notes):
+    for key, value in NOTE_DIC.items():
+        notes = notes.replace(key, value)
+    return notes
 
 def solution(m, musicinfos):
-    VERBOSE = True
-    def log(*msg):
-        if VERBOSE: print(*msg)
-    # -----------------------------
+    # 1. Rearrange music info: str -> int
+    m_cvt = convert(m)
+    data = {}
+    for start_time, end_time, name, notes_unit in map(lambda musicinfo: musicinfo.split(','), musicinfos):
+        duration       = (datetime.strptime(end_time, "%H:%M") - datetime.strptime(start_time, "%H:%M")).seconds // 60
+        notes_unit_cvt = convert(notes_unit)
+        notes          = ''.join(islice(cycle(notes_unit_cvt), duration))
+        data[name]     = [duration, start_time, notes]
 
-    m = re.sub(r'([A-Z])#', lambda m: m.group(1).lower(), m)
+    # 2. Find all matches
     cands = []
-    for start, end, name, music in (musicinfo.split(',') for musicinfo in musicinfos):
-        # 1. Process data
-        music = re.sub(r'([A-Z])#', lambda m: m.group(1).lower(), music)
-        times = (datetime.strptime(end, "%H:%M") - datetime.strptime(start, "%H:%M")).seconds // 60
+    for name, (duration, start_time, notes) in data.items():
+        if m_cvt in notes:
+            cands.append([duration, start_time, name])
 
-        # 2. Reconstruct full notes
-        music = cycle(music)
-        music_full = ''.join(next(music) for i in range(times))
-        log(music_full)
-
-        # 3. Search substring
-        if re.search(m, music_full) is not None:
-            cands.append([times, datetime.strptime(start, "%H:%M").timestamp(), name])
-
-    # 4. Select answer
-    if len(cands) == 0:
-        return '(None)'
-
-    return max(cands, key=lambda x: (x[0], -x[1]))[2]
+    if len(cands):
+        return min(cands, key=lambda x: (-x[0], x[1]))[2]  # max(duration), min(start_time)
+    else:
+        return "(None)"
 
 
-m = "ABC"
-musicinfos = ["12:00,12:14,HELLO,C#DEFGAB",
-              "13:00,13:05,WORLD,ABCDEF"]
-print(solution(m, musicinfos))
+args = [
+    # "ABCDEFG",	["12:00,12:14,HELLO,CDEFGAB",
+    #              "13:00,13:05,WORLD,ABCDEF"]
+    "CC#BCC#BCC#BCC#B",	["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
+]
+print(solution(*args))
